8d4e4ce614a5ad657902edb210fbcc3d211d766b
jserov96 <jserov96@gmail.com>
2025-12-28 23:20:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -272,7 +272,7 @@ if __name__ == "__main__":
     ru_files = save_chunked(res_ru_clean, FOLDER_RU, "ru_white")
     euro_files = save_chunked(res_euro_clean, FOLDER_EURO, "my_euro")
 
-    GITHUB_USER_REPO = "kort0881/vpn-checker-backend"
+    GITHUB_USER_REPO = "jserov96/vpn-checker-backend"
     BRANCH = "main"
     BASE_URL_RU = f"https://raw.githubusercontent.com/{GITHUB_USER_REPO}/{BRANCH}/{BASE_DIR}/RU_Best"
     BASE_URL_EURO = f"https://raw.githubusercontent.com/{GITHUB_USER_REPO}/{BRANCH}/{BASE_DIR}/My_Euro"
@@ -300,6 +300,7 @@ if __name__ == "__main__":
 
     print(f"Ссылки сформированы: RU={len(ru_files)}, EURO={len(euro_files)}")
     print("=== DONE SUCCESS ===")
+
 
 
 
